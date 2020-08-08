d71b35dac19e39e6ab7e14171702a31ba0803707
Meredith Myers <meredith@myers1.com>
2020-08-07 23:37:23 -0500

    Implemented functionality to add generated data to database

--- database/seed.js
+++ database/seed.js
@@ -1,21 +1,27 @@
 const faker = require('faker');
+const mysql = require('mysql');
+const config = require('./config.js')
+
+const connection = mysql.createConnection(config);
+
+connection.connect();
+
+let restaurantData = {
+  restaurantName: '',
+  dishName1: '',
+  dishImage1: '',
+  dishName2: '',
+  dishImage2: '',
+  dishName3: '',
+  dishImage3: '',
+  tip: '',
+  features: [],
+  tags: []
+};
 
 let restaurantEntries = [];
 
 function generateIndividualRestaurantData() {
-
-  let restaurantData = {
-    restaurantName: '',
-    dishName1: '',
-    dishImage1: '',
-    dishName2: '',
-    dishImage2: '',
-    dishName3: '',
-    dishImage3: '',
-    tip: '',
-    features: [],
-    tags: []
-  };
 
   restaurantData['restaurantName'] = faker.company.companyName();
 
@@ -38,6 +44,7 @@ function generateIndividualRestaurantData() {
 
   while (restaurantData['features'].length < randomInteger) {
     restaurantData['features'].push(faker.commerce.productAdjective());
+    // console.log(restaurantData['features']);
   }
 
   randomInteger = getRandomInteger(1, 20);
@@ -46,14 +53,50 @@ function generateIndividualRestaurantData() {
     restaurantData['tags'].push(faker.commerce.productAdjective());
   }
 
-  restaurantEntries.push(restaurantData);
+  return restaurantData;
 }
 
-function generateBulkRestaurantData(totalNumberOfEntries) {
-  while (restaurantEntries.length < totalNumberOfEntries) {
-    generateIndividualRestaurantData();
-    console.log(restaurantEntries);
-  }
+for (restaurantCount = 0; restaurantCount < 100; restaurantCount++) {
+  let restaurantObject = generateIndividualRestaurantData();
+
+  let columns = `restaurant_name, dish_name1, dish_image1, dish_name2, dish_image2, dish_name3, dish_image3, tip, features, tags`;
+  let values = '"' + restaurantData['restaurantName'] + '", ' + '"' + restaurantData['dishName1'] + '", ' + '"' + restaurantData['dishImage1'] + '", ' + '"' + restaurantData['dishName2'] + '", ' + '"' + restaurantData['dishImage2'] + '", ' + '"' + restaurantData['dishName3'] + '", ' + '"' + restaurantData['dishImage3'] + '", ' + '"' + restaurantData['tip'] + '", ' + '"' + restaurantData['features'] + '", ' + '"' + restaurantData['tags'] + '"'
+  console.log(values);
+
+  connection.query("INSERT INTO Restaurants (" + columns + ") VALUES (" + values + ")", (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      console.log(results);
+    });
 }
 
-generateBulkRestaurantData(100);
+// for (var i = 0; i < restaurantEntries.length; i++) {
+//   // console.log(restaurantEntries);
+//   console.log(restaurantEntries[0]);
+//   let currentRestaurantName = restaurantEntries[i]['restaurantName'];
+//   let currentDishName1 = restaurantEntries[i]['dishName1'];
+//   let currentDishImage1 = restaurantEntries[i]['dishImage1'];
+//   let currentDishName2 = restaurantEntries[i]['dishName2'];
+//   let currentDishImage2 = restaurantEntries[i]['dishImage2'];
+//   let currentDishName3 = restaurantEntries[i]['dishName3'];
+//   let currentDishImage3 = restaurantEntries[i]['dishImage3'];
+//   let currentTip = restaurantEntries[i]['tip'];
+//   let currentFeatures = restaurantEntries[i]['features'];
+//   let currentTags = restaurantEntries[i]['tags'];
+
+//   let columns = `restaurant_name, dish_name1, dish_image1, dish_name2, dish_image2, dish_name3, dish_image3, tip, features, tags`;
+
+//   let values = currentRestaurantName + ', ' + currentDishName1 + ', ' + currentDishImage1 + ', ' + currentDishName2 + ', ' + currentDishImage2 + ', ' + currentDishName3 + ', ' + currentDishImage3 + ', ' + currentTip + ', ' + currentFeatures + ', ' + currentTags;
+
+//   connection.query("INSERT INTO Restaurants (" + columns + ") VALUES (" + values + ")", (error, results, fields) => {
+//     //   if (error) {
+//     //     console.log(error);
+//     //     return;
+//     //   }
+
+//     //   console.log(results);
+//     });
+// }
