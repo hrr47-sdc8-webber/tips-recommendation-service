8ee31c9526aa12e67ae5e3e9238bc4a8770cef0a
Meredith Myers <meredith@myers1.com>
2020-08-12 17:45:16 -0500

    Updated linter setup and set up structure for front end

---
+++ client/App.jsx
@@ -0,0 +1,24 @@
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return ();
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
