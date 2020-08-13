346fdc52237ed22a8a36847c372c04e7e92b39d0
Meredith Myers <meredith@myers1.com>
2020-08-13 17:51:22 -0500

    Made note to not render sections without data

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
+// TO DO:
+  // IF NO ARTICLES FOR RESTAURANT EXIST, DO NOT INCLUDE ARTICLES SECTION OF PAGE
 
 class App extends React.Component {
 
