d86836eaa7bbbb5b2ae0f9fadb90b1e53ab73a4c
Meredith Myers <meredith@myers1.com>
2020-08-13 09:17:57 -0500

    Implemented boilerplate tests to be updated once front-end built out more

---
+++ testing/App.test.js
@@ -0,0 +1,39 @@
+// TESTS RENDERING
+
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import App from "../client/App.jsx";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+// EXAMPLE TO BE UPDATED ONCE FRONT-END BUILT
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Hello />, container);
+  });
+  expect(container.textContent).toBe("Hey, stranger");
+
+  act(() => {
+    render(<Hello name="Jenny" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Jenny!");
+
+  act(() => {
+    render(<Hello name="Margaret" />, container);
+  });
+  expect(container.textContent).toBe("Hello, Margaret!");
+});
