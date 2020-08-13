3849a14800b8976e6e9e560cf44873cc64a2a6e9
Meredith Myers <meredith@myers1.com>
2020-08-13 10:13:40 -0500

    Updated, installed, etc. package and cleaned up test files

--- testing/App.test.js
+++ testing/App.test.js
@@ -2,39 +2,63 @@
 
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
+import {describe, expect, test} from '@jest/globals'
+import sinon from 'sinon';
+import App from '../client/App.jsx';
 
-import Foo from '../client/App.jsx';
+// TO DO: IMPORT ADDITIONAL COMPONENTS
 
-const wrapper = shallow(<Foo />);
+// SHALLOW RENDERING
+  // TO DO: REPLACE DETAILS ONCE FRONTEND IS IMPLEMENTED
+describe('<App />', () => {
+  it('renders three <Foo /> components', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Foo)).to.have.lengthOf(3);
+  });
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
+  it('renders an `.icon-star`', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
+  });
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <App>
+        <div className="unique" />
+      </App>
+    ));
+    expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+  });
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = shallow(<App onButtonClick={onButtonClick} />);
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
 });
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-// EXAMPLE TO BE UPDATED ONCE FRONT-END BUILT
-it("renders with or without a name", () => {
-  act(() => {
-    render(<Hello />, container);
+// FULL RENDERING
+describe('<App />', () => {
+  it('calls componentDidMount', () => {
+    sinon.spy(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />);
+    expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
   });
-  expect(container.textContent).toBe("Hey, stranger");
 
-  act(() => {
-    render(<Hello name="Jenny" />, container);
+  it('allows us to set props', () => {
+    const wrapper = mount(<App bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
   });
-  expect(container.textContent).toBe("Hello, Jenny!");
 
-  act(() => {
-    render(<Hello name="Margaret" />, container);
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount((
+      <App onButtonClick={onButtonClick} />
+    ));
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
   });
-  expect(container.textContent).toBe("Hello, Margaret!");
 });
