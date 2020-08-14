9c4140a99289cbf35c40aaa32760944da1be4515
Meredith Myers <meredith@myers1.com>
2020-08-14 14:45:30 -0500

    Implemented What to Order structure

--- client/App.jsx
+++ client/App.jsx
@@ -17,7 +17,28 @@
     }
 
     render() {
-      return ();
+      return (
+
+        <section id='what-to-order'>
+          <h3>WHAT TO ORDER</h3>
+          <figure class='dish'>
+            <img src="" alt="">
+            <figcaption>DISH NAME</figcaption>
+          </figure>
+          <figure class='dish'>
+            <img src="" alt="">
+            <figcaption>DISH NAME</figcaption>
+          </figure>
+          <figure class='dish'>
+            <img src="" alt="">
+            <figcaption>DISH NAME</figcaption>
+          </figure>
+        </section>
+
+        <section id='insider-tip'></section>
+        <section id='known-for'></section>
+        <section id='articles'></section>
+      );
     }
   }
 
