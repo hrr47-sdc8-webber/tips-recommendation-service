6e818f8ea9db353c1ea4107e573d81fd937f7f6f
Meredith Myers <meredith@myers1.com>
2020-08-14 15:18:52 -0500

    Fixed formatting for App file

--- client/App.jsx
+++ client/App.jsx
@@ -1,77 +1,77 @@
 // TO DO:
   // IF NO ARTICLES FOR RESTAURANT EXIST, DO NOT INCLUDE ARTICLES SECTION OF PAGE
 
-  class App extends React.Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-    constructor(props) {
-      super(props);
-      this.state = '';
-    }
-
-    componentDidMount() {
-
-    }
-
-    componentWillUnmount() {
-
-    }
-
-    render() {
-      return (
-
-        <section id='what-to-order'>
-          <h3>WHAT TO ORDER</h3>
-          <figure class='dish'>
-            <img src='' alt=''>
-            <figcaption>DISH NAME</figcaption>
-          </figure>
-          <figure class='dish'>
-            <img src='' alt=''>
-            <figcaption>DISH NAME</figcaption>
-          </figure>
-          <figure class='dish'>
-            <img src='' alt=''>
-            <figcaption>DISH NAME</figcaption>
-          </figure>
-        </section>
-
-        <section id='insider-tip'>
-          <h3>INSIDER TIP</h3>
-          <hr>
-          <p>RANDOM TEXT HERE</p>
-          <hr>
-        </section>
-
-        <section id='known-for'>
-          <figure class='feature'>
-            <img src='' alt=''>
-            <figcaption>FEATURE</figcaption>
-          </figure>
-          <figure class='feature'>
-            <img src='' alt=''>
-            <figcaption>FEATURE</figcaption>
-          </figure>
-          <figure class='feature'>
-            <img src='' alt=''>
-            <figcaption>FEATURE</figcaption>
-          </figure>
-        </section>
-
-        <section id='articles'>
-          <figure class='article'>
-            <img src='' alt=''>
-            <figcaption>ARTICLE</figcaption>
-          </figure>
-          <figure class='article'>
-            <img src='' alt=''>
-            <figcaption>ARTICLE</figcaption>
-          </figure>
-        </section>
-      );
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
   }
 
-  ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-  );
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      <section id='what-to-order'>
+        <h3>WHAT TO ORDER</h3>
+        <figure class='dish'>
+          <img src='' alt=''/>
+          <figcaption>DISH NAME</figcaption>
+        </figure>
+        <figure class='dish'>
+          <img src='' alt=''/>
+          <figcaption>DISH NAME</figcaption>
+        </figure>
+        <figure class='dish'>
+          <img src='' alt=''/>
+          <figcaption>DISH NAME</figcaption>
+        </figure>
+      </section>
+
+      <section id='insider-tip'>
+        <h3>INSIDER TIP</h3>
+        <hr/>
+        <p>RANDOM TEXT HERE</p>
+        <hr/>
+      </section>
+
+      <section id='known-for'>
+        <figure class='feature'>
+          <img src='' alt=''/>
+          <figcaption>FEATURE</figcaption>
+        </figure>
+        <figure class='feature'>
+          <img src='' alt=''/>
+          <figcaption>FEATURE</figcaption>
+        </figure>
+        <figure class='feature'>
+          <img src='' alt=''/>
+          <figcaption>FEATURE</figcaption>
+        </figure>
+      </section>
+
+      <section id='articles'>
+        <figure class='article'>
+          <img src='' alt=''/>
+          <figcaption>ARTICLE</figcaption>
+        </figure>
+        <figure class='article'>
+          <img src='' alt=''/>
+          <figcaption>ARTICLE</figcaption>
+        </figure>
+      </section>
+    );
+  }
+}
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
