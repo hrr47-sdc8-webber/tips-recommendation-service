1a739412c64ca3f623150d47bc9781d6f0c91548
Meredith Myers <meredith@myers1.com>
2020-08-17 01:26:17 -0500

    Implemented KnownFor with styling

--- client/src/KnownFor.jsx
+++ client/src/KnownFor.jsx
@@ -2,6 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+const KnownForContainer = styled.div`
+  width: 100%;
+  margin: 0px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const StyledFeature = styled.figure`
+  display: flex;
+  align-items: baseline;
+`;
+
+const KnownForIcon = styled.img`
+  width: 96px;
+  height: 72px;
+`;
+
+const KnownForFont = styled.figcaption`
+  color: #101820;
+  font-size: 15px;
+  letter-spacing: .013em;
+  text-align: center;
+`;
+
+const icons = ['../dist/icons/kids.svg', '../dist/icons/lunch.svg', '../dist/icons/placeholder.svg', '../dist/icons/takeout.svg'];
+
 class KnownFor extends React.Component {
   constructor(props) {
     super(props);
@@ -9,15 +36,14 @@ class KnownFor extends React.Component {
 
   render() {
     return (
-      <div>
-        {console.log(this.props.features.split(','))}
+      <KnownForContainer>
         {this.props.features.split(',').map((feature) => (
           <figure class='feature'>
-            <img src='' alt=''/>
-            <figcaption>{feature}</figcaption>
+            <KnownForIcon src={icons[Math.floor(Math.random() * (4))]} alt='icon'/>
+            <KnownForFont>{feature}</KnownForFont>
           </figure>
         ))}
-      </div>
+      </KnownForContainer>
     );
   }
 }
