ead83c9c62986e052c861b7af049901406d978c0
Chandra Nguyen <chandra.nguyen@outlook.com>
2020-09-09 15:13:21 -0600

    Now baseURL should be fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const axios = require('axios');
 const app = express();
 const port = 3003;
 
-const instance = axios.create({ baseURL: "http://" + location. host. split(":")[0] });
+var getUrl = window.location;
+var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+
+const instance = axios.create({ baseURL: baseUrl });
 
 app.use(cors());
 app.use(express.json());
